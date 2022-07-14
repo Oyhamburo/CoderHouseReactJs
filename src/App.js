e08b096a070d6589aedaa79c12730d8cf697500e
Jeremias Aitor <oyhamburo.jeremias@gmail.com>
2022-07-14 20:43:39 -0300

    primer commit ReactJs

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Jeremias Aitor Oyhamburo
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto ecommerce Cortinas Roller
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
